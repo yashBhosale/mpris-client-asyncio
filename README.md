80b9de8430cd103c31eae3f99bfaec05ec3d955d
notYash <notyash@localhost.localdomain>
2021-04-02 04:39:47 -0400

    moving stuff around for packaging

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mpris-client-asyncio
+
+This is a client-side library for the MPRIS Protocol, written to work with asyncio. 
+
+
+
+Suggestions and pull requests welcome! 
--- setup.py
+++ setup.py
@@ -4,19 +4,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="asyncio-mpris-client",
-    version="1.0",
-    author="yashBhosale",
+    name="mpris_client_asyncio",
+    version="1.1",
+    author="Yash Bhosale",
     author_email="bhosaley5@gmail.com",
     description="Client library for MPRIS that uses asyncio",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yashBhosale/mpris-client-asyncio",
-    packages=setuptools.find_packages(),
+    project_urls={
+        "Bug Tracker": "https://github.com/yashBhosale/mpris-client-asyncio/issues",
+    },
     classifiers=[       
         "Operating System :: POSIX :: Linux",
         "License :: OSI Approved :: MIT License",
         "Framework :: AsyncIO",
     ],
     python_requires='>=3.6',
+
+    packages=["mpris_client_asyncio"]
 )
